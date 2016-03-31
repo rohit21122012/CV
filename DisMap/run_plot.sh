27aeb97cca3b21351f2bc74b701b008fae3a9413
rohit21122012 <rohit21122012@gmail.com>
2016-03-31 10:10:38 +0530

    reran with 3*3 window

---
+++ DisMap/run_plot.sh
@@ -0,0 +1,3 @@
+echo "PlotDM('pair1/')" |  nohup  matlab2013 -nodesktop -singleCompThread > plot1.txt &
+echo "PlotDM('pair2/')" |  nohup  matlab2013 -nodesktop -singleCompThread > plot2.txt &
+echo "PlotDM('pair3/')" |  nohup  matlab2013 -nodesktop -singleCompThread > plot3.txt &
