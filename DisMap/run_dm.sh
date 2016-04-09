b901117467d7af80273d229a991927e188b53929
Rohit <rohit21122012@gmail.com>
2016-04-09 08:16:47 +0530

    Run with 32-64 space

--- DisMap/run_dm.sh
+++ DisMap/run_dm.sh
@@ -1,6 +1,8 @@
-echo "DisparityMap('pair1/', 'pair1/scene_l.ppm', 'pair1/scene_r.ppm',2, 20)" | nohup matlab2013 -nodesktop -singleCompThread > pair1/out20.txt &
-echo "DisparityMap('pair1/','pair1/scene_l.ppm', 'pair1/scene_r.ppm',2, 15)" | nohup matlab2013 -nodesktop -singleCompThread > pair1/out15.txt &
-echo "DisparityMap('pair2/','pair2/scene_l.png', 'pair2/scene_r.png',2,20)" | nohup matlab2013 -nodesktop -singleCompThread > pair2/out20.txt 2>pair2/err20.txt &
-echo "DisparityMap('pair2/','pair2/scene_l.png', 'pair2/scene_r.png',2,15)" | nohup matlab2013 -nodesktop -singleCompThread > pair2/out15.txt &
- echo "DisparityMap('pair3/','pair3/im_l.png', 'pair3/im_r.png',2,20)" | nohup matlab2013 -nodesktop -singleCompThread > pair3/out20.txt &
- echo "DisparityMap('pair3/','pair3/im_l.png', 'pair3/im_r.png',2,15)" | nohup matlab2013 -nodesktop -singleCompThread > pair3/out15.txt &
+echo "DisparityMap('pair1/', 'pair1/scene_l.ppm', 'pair1/scene_r.ppm',2, 64)" | nohup matlab2013 -nodesktop -singleCompThread > pair1/out64.txt &
+echo "DisparityMap('pair1/','pair1/scene_l.ppm', 'pair1/scene_r.ppm',2, 32)" | nohup matlab2013 -nodesktop -singleCompThread > pair1/out32.txt &
+
+echo "DisparityMap('pair2/','pair2/scene_l.png', 'pair2/scene_r.png',2,64)" | nohup matlab2013 -nodesktop -singleCompThread > pair2/out64.txt &
+echo "DisparityMap('pair2/','pair2/scene_l.png', 'pair2/scene_r.png',2,32)" | nohup matlab2013 -nodesktop -singleCompThread > pair2/out32.txt &
+echo "DisparityMap('pair3/','pair3/im_l.png', 'pair3/im_r.png',2,64)" | nohup matlab2013 -nodesktop -singleCompThread > pair3/out64.txt &
+echo "DisparityMap('pair3/','pair3/im_l.png', 'pair3/im_r.png',2,32)" | nohup matlab2013 -nodesktop -singleCompThread > pair3/out32.txt &
+
